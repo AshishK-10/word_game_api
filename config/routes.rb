26724bb17d07ee1eaf224f1489c1064c63d32f26
AshishK-10 <77068004+AshishK-10@users.noreply.github.com>
2022-07-26 15:02:53 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,8 @@ Rails.application.routes.draw do
   get 'fortytwoword/:word/example',to: "fortytwoword#example" 
   get 'fortytwoword/:word/defination',to: "fortytwoword#defination" 
   get 'fortytwoword/:word/relation',to: "fortytwoword#wordRelation" 
-
-  resources :fortytwoword do
-    match '*unmatched', to: 'application#route_not_found', via: :all
-end
+  match '*unmatched', to: 'application#route_not_found', via: :all
+  
 get 'home/profile'
   root "home#index"
 
