4a008fe2879bd818ac92fa47638cf42bee0c7367
ashish <kumarashish2615@gmail.com>
2022-08-01 16:10:45 +0530

    changes in model and controller

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,7 @@
 class Word < ApplicationRecord
+  serialize :defination
+  serialize :example
+  serialize :relationshipType
   after_initialize do |word|
     word.defination= [] if word.defination == nil
     word.example= [] if word.example == nil
